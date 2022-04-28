db01660710cb413b94b54ae2f06f7b4fa0add8b4
keza681 <lindakeza19@gmail.com>
2022-04-28 11:56:54 +0200

    Add details component to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import NavBar from './components/Navbar';
+import HomePage from './components/HomePage';
+import Details from './components/Details';
+import { getCountries } from './redux/reducers/covidReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getCountries()(dispatch);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<Details />} />
+      </Routes>
     </div>
+
   );
 }
 
